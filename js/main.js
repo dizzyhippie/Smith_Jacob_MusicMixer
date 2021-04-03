0eab2ae3e9d14d8a9ca4ef1d74c229433685bd39
Jacob Smith <77408757+dizzyhippie@users.noreply.github.com>
2021-04-03 16:07:05 -0400

    Drag and Drop Play working

--- js/main.js
+++ js/main.js
@@ -7,7 +7,6 @@ const instrumentIcons = document.querySelectorAll(".instruments"),
 	playBoardContainer = document.querySelector("#dropzone_container");
 
 
-
 //Click & Held onto an icon
 function picked(){
 	console.log('You have chosen an Instrument');
@@ -34,18 +33,14 @@ function chosen(event){
 	event.target.appendChild(document.querySelector(`#${targetID}`));
 }
 
-
 function playSound(event){
 	let soundChoice = event.dataTransfer.getData("soundChoice");
 	let audio = document.querySelector(`audio[data-sound="${soundChoice}"]`);
-
-	console.log("you chose", soundChoice)
+	console.log("you chose", soundChoice);
 
 	audio.currentTime= 0;
 	audio.play();
-
 }
-
 
 //Event Handling at the bottom
 instrumentIcons.forEach(piece => piece.addEventListener("dragstart", picked));
