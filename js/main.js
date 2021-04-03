e52308a8be2300a1e3bdc7696c698ba948060c10
Jacob Smith <77408757+dizzyhippie@users.noreply.github.com>
2021-04-03 15:33:34 -0400

    Working Audio for Radiant Horizon

--- js/main.js
+++ js/main.js
@@ -9,11 +9,12 @@ const instrumentIcons = document.querySelectorAll(".instruments"),
 
 
 //Click & Held onto an icon
-function dragStart(){
+function picked(){
 	console.log('You have chosen an Instrument');
 
 	//Reparent the instrument to the drop zone
 	event.dataTransfer.setData("savedID", this.id);
+	event.dataTransfer.setData("soundChoice", this.dataset.sound);
 
 }
 
@@ -24,22 +25,35 @@ function draggedOver(event){
 }
 
 //Dropped onto the playboard
-function dropped(event){
+function chosen(event){
 	event.preventDefault();
-	console.log("dropped");
 	let targetID = event.dataTransfer.getData("savedID");
-	console.log("I chose this one", targetID);
+	console.log("I chose this", targetID);
 
 	//Move the icon into the selected "drop" container
 	event.target.appendChild(document.querySelector(`#${targetID}`));
 }
 
 
+function playSound(event){
+	let soundChoice = event.dataTransfer.getData("soundChoice");
+	let audio = document.querySelector(`audio[data-sound="${soundChoice}"]`);
+
+	console.log("you chose", soundChoice)
+
+	audio.currentTime= 0;
+	audio.play();
+
+}
+
+
 //Event Handling at the bottom
-instrumentIcons.forEach(piece => piece.addEventListener("dragstart", dragStart));
+instrumentIcons.forEach(piece => piece.addEventListener("dragstart", picked));
 playBoard.forEach(zone => {
 	zone.addEventListener("dragover", draggedOver);
-	zone.addEventListener("drop", dropped);
+	zone.addEventListener("drop", chosen);
+
+window.addEventListener("drop", playSound);
 
 })
 
