355f359921d7f0be1f25c31fd7cfdc542a78bf78
Jacob Smith <jacob.regsmith@gmail.com>
2021-04-08 19:42:22 -0400

    some bug crushing and updates

--- js/main.js
+++ js/main.js
@@ -6,9 +6,10 @@ const instrumentIcons = document.querySelectorAll(".instruments"),
 	playBoard = document.querySelectorAll(".dropzones"), 
 	playBoardContainer = document.querySelector("#dropzone_container"),
 	footerSounds = document.querySelectorAll(".bot_instruments"),
+	playButton = document.querySelector("#play"),
+	pauseButton = document.querySelector("#pause"),
 	resetButton =document.querySelector("#reset"),
 	startOverButton = document.querySelector("#startOver");
-
 
 //Click & Held onto an icon
 function picked(){
@@ -29,13 +30,14 @@ function chosen(event){
 	let targetID = event.dataTransfer.getData("savedID");
 	console.log("I chose this", targetID);
 
-	//Move the icon into the selected "drop" container
+	//Move the icon into the selected "drop" containers
 	event.target.appendChild(document.querySelector(`#${targetID}`));
 }
 
 function playSound(event){
 	let soundChoice = event.dataTransfer.getData("soundChoice");
 	let audio = document.querySelector(`audio[data-sound="${soundChoice}"]`);
+	audio.classList.add("playing");
 	console.log("you chose", soundChoice);
 	audio.currentTime = 0;
 	audio.play();
@@ -53,6 +55,14 @@ function startOver(event){
 	window.location.reload();
 }
 
+function pauseTrack(event){
+	event.preventDefault();
+	console.log("pausing track");
+}
+
+//AUDIO VOLUME THING
+// audio.volume = 0.5;
+
 //Event Handling at the bottom
 instrumentIcons.forEach(piece => piece.addEventListener("dragstart", picked));
 footerSounds.forEach(piece => piece.addEventListener("dragstart", picked));
@@ -60,7 +70,8 @@ playBoard.forEach(zone => {
 	zone.addEventListener("dragover", draggedOver);
 	zone.addEventListener("drop", chosen);
 	zone.addEventListener("drop", restartTrack);	
-})
+});
+pauseButton.addEventListener("click", pauseTrack);
 resetButton.addEventListener("click", restartTrack);
 startOverButton.addEventListener("click", startOver);
 window.addEventListener("drop", playSound);
