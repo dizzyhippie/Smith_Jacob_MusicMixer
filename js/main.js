bbc472bcff4fba13e8d864a8264dcc8ec679bfbe
Jacob Smith <jacob.regsmith@gmail.com>
2021-04-08 18:35:07 -0400

    All audio added, restart button and timing fixed.
    audio looping, audio synching (restart on drop) and refresh option added

--- js/main.js
+++ js/main.js
@@ -5,17 +5,16 @@ const instrumentIcons = document.querySelectorAll(".instruments"),
 	loops = document.querySelectorAll(".loops"), 
 	playBoard = document.querySelectorAll(".dropzones"), 
 	playBoardContainer = document.querySelector("#dropzone_container"),
-	footerSounds = document.querySelectorAll(".bot_instruments");
+	footerSounds = document.querySelectorAll(".bot_instruments"),
+	resetButton =document.querySelector("#reset"),
+	startOverButton = document.querySelector("#startOver");
 
 
 //Click & Held onto an icon
 function picked(){
 	console.log('You have chosen an Instrument');
-
-	//Reparent the instrument to the drop zone
 	event.dataTransfer.setData("savedID", this.id);
 	event.dataTransfer.setData("soundChoice", this.dataset.sound);
-
 }
 
 //Dragged over the playboard
@@ -38,9 +37,20 @@ function playSound(event){
 	let soundChoice = event.dataTransfer.getData("soundChoice");
 	let audio = document.querySelector(`audio[data-sound="${soundChoice}"]`);
 	console.log("you chose", soundChoice);
-
-	audio.currentTime= 0;
+	audio.currentTime = 0;
 	audio.play();
+}
+
+//Use when a new track is added, or they hit the restart button
+function restartTrack(event){
+	event.preventDefault();
+	let track = document.querySelector('audio');
+	console.log("restarting track");
+	track.currentTime = 0;
+}
+
+function startOver(event){
+	window.location.reload();
 }
 
 //Event Handling at the bottom
@@ -49,10 +59,11 @@ footerSounds.forEach(piece => piece.addEventListener("dragstart", picked));
 playBoard.forEach(zone => {
 	zone.addEventListener("dragover", draggedOver);
 	zone.addEventListener("drop", chosen);
-
-window.addEventListener("drop", playSound);
-
+	zone.addEventListener("drop", restartTrack);	
 })
+resetButton.addEventListener("click", restartTrack);
+startOverButton.addEventListener("click", startOver);
+window.addEventListener("drop", playSound);
 
 })();
 
